8970e52e4ff0e6d60cf7690562d77a99023a0e51
Hendson <hendsonszeto@live.com>
2022-09-10 15:03:12 -0700

    Change the RequestMapping endpoint

--- src/main/java/com/itshendson/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/itshendson/studentmanagementsystem/controller/StudentController.java
@@ -3,14 +3,12 @@ package com.itshendson.studentmanagementsystem.controller;
 import com.itshendson.studentmanagementsystem.entity.Student;
 import com.itshendson.studentmanagementsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1")
 public class StudentController {
 
     @Autowired
@@ -20,14 +18,14 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    @PostMapping("/students")
-    public Student saveStudent(@RequestBody Student student) {
-        return studentService.saveStudent(student);
-    }
-
-    @GetMapping("/students")
+    @GetMapping("/student")
     public List<Student> fetchStudentList() {
         return studentService.fetchStudentList();
+    }
+
+    @PostMapping("/student")
+    public Student saveStudent(@RequestBody Student student) {
+        return studentService.saveStudent(student);
     }
 
     @GetMapping("/")
