37d9ab7f61467e7e350e1e86e3e54c7d5a6b17b5
Hendson <hendsonszeto@live.com>
2022-09-17 23:47:31 -0700

    Implement unit and mock testing for StudentService

--- src/test/java/com/itshendson/studentmanagementsystem/service/StudentServiceTest.java
+++ src/test/java/com/itshendson/studentmanagementsystem/service/StudentServiceTest.java
@@ -1,43 +1,119 @@
 package com.itshendson.studentmanagementsystem.service;
 
+import com.itshendson.studentmanagementsystem.entity.Faculty;
+import com.itshendson.studentmanagementsystem.entity.Student;
+import com.itshendson.studentmanagementsystem.exception.StudentNotFoundException;
 import com.itshendson.studentmanagementsystem.repository.StudentRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
 
     @Mock
-    private StudentRepository studentRepository;
-    private StudentService underTest;
-    private AutoCloseable autoCloseable;
+    private StudentRepository studentRepositoryMock;
+    @InjectMocks
+    private StudentService studentServiceTest;
+    private Student studentOne;
+    private Student studentTwo;
+    private Student studentThree;
 
     @BeforeEach
     void setUp() {
-        // If we have more than one mock test, it'll initialize them in this test class.
-        autoCloseable = MockitoAnnotations.openMocks(this);
-        underTest = new StudentService(studentRepository);
+        studentOne = Student.builder()
+                .studentId(1L)
+                .firstName("French Fries")
+                .lastName("McGovern")
+                .faculty(Faculty.BUSINESS)
+                .build();
+
+        studentTwo = Student.builder()
+                .studentId(2L)
+                .firstName("Glizzy")
+                .lastName("Golden")
+                .faculty(Faculty.COMMUNICATION)
+                .build();
+
+        studentThree = Student.builder()
+                .studentId(3L)
+                .firstName("French Fries")
+                .lastName("Salty")
+                .faculty(Faculty.MUSIC)
+                .build();
     }
 
-    @AfterEach
-    void tearDown() throws Exception {
-        autoCloseable.close(); // Closes our resource after the test.
-    }
-
-    @DisplayName("Get all students")
+    @DisplayName("When StudentService invokes saveStudent(), studentRepository should invoke save()")
     @Test
-    void fetchStudentList() {
+    void saveValidStudentTest() {
         // when
-        underTest.fetchStudentList();
-
+        when(studentRepositoryMock.save(studentOne)).thenReturn(studentOne);
         // then
-        verify(studentRepository).findAll(); // verify that studentRepository was ran by invoking findAll()
+        assertEquals(studentOne, studentServiceTest.saveStudent(studentOne));
+        verify(studentRepositoryMock).save(studentOne);
+        verify(studentRepositoryMock, times(1)).save(studentOne);
+    }
 
+    @Disabled
+    @DisplayName("Save student that already exists in repository.")
+    @Test
+    void saveDuplicateStudentTest() {
+        // Implement this
+    }
+
+    @DisplayName("When StudentService invokes fetchStudentList(), studentRepository should invoke findAll()")
+    @Test
+    void fetchStudentListTest() {
+        // given
+        List<Student> students = Arrays.asList(studentOne);
+        // when
+        when(studentRepositoryMock.findAll()).thenReturn(students);
+        // then
+        assertEquals(students, studentServiceTest.fetchStudentList());
+        verify(studentRepositoryMock, times(1)).findAll();
+    }
+
+    @DisplayName("Fetch existent student by ID")
+    @Test
+    void fetchStudentByValidIdTest() {
+        // when
+        when(studentRepositoryMock.findById(1L)).thenReturn(Optional.ofNullable(studentOne));
+        // then
+        assertEquals(studentOne, studentServiceTest.fetchStudentById(1L));
+        verify(studentRepositoryMock, times(2)).findById(1L);
+    }
+
+    @DisplayName("Fetch non-existent student by ID")
+    @Test
+    void fetchStudentByNonExistentIdTest() {
+        // when
+        doThrow(new StudentNotFoundException("Student ID " + 99L + " does not exist.")).when(studentRepositoryMock).findById(99L);
+        // then
+        assertThrows(StudentNotFoundException.class, () -> studentServiceTest.fetchStudentById(99L));
+        verify(studentRepositoryMock, times(1)).findById(99L);
+    }
+
+    @DisplayName("Fetch all students by first name regardless of case")
+    @Test
+    void fetchStudentByValidFirstName() {
+        // given
+        List<Student> studentsNamedFrenchFries = Arrays.asList(studentOne, studentThree);
+        // when
+        when(studentRepositoryMock.findByFirstNameIgnoreCase("fReNcH FrIEs")).thenReturn(studentsNamedFrenchFries);
+        // then
+        assertEquals(studentsNamedFrenchFries, studentServiceTest.fetchAllStudentsByFirstName("fReNcH FrIEs"));
+        verify(studentRepositoryMock, times(1)).findByFirstNameIgnoreCase("fReNcH FrIEs");
     }
 }
